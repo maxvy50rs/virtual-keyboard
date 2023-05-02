fe82b7b3253c57a4f44ec612967668cecc7fc8f8
maxvy50rs <vysokov.maksim95@yandex.ru>
2023-05-02 19:24:29 +0600

    deploy: build and deploy final version

--- main.js
+++ main.js
@@ -21,5 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const testArea = document.getElementById('test-area');
   keyboard.bindTo(testArea);
   keyboard.init();
-  alert('Привет! Я из-за работы не успел доделать некоторые моменты (описал в PR) -- буду признателен, если дашь мне еще немного времени и перепроверишь. Thanks!');
 });
